36d092f9391948bf514929d1854de6a99664825d
Sofia Donato Ferreira <flowlnlnln@gmail.com>
2026-05-23 18:08:07 -0300

    fix: set expiration timeout after notification acquires new id
    Signed-off-by: Sofia Donato Ferreira <flowlnlnln@gmail.com>

--- crates/bell/src/dbus.rs
+++ crates/bell/src/dbus.rs
@@ -362,17 +362,16 @@ fn handle_notify_message(
     // The server must atomically (ie with no flicker or other visual cues) replace
     // the given notification with this one.
     let mut id = input.replaces_id;
-
-    if let Some(timeout) = expire_timeout {
-        notification_manager.set_timeout_for_notification(&id, timeout);
-    }
-
     if id == 0 {
         // If replaces_id is 0, the return value is a UINT32 that represent the notification.
         id = notification_manager.add_notification(notification);
     } else {
         // If replaces_id is not 0, the returned value is the same value as replaces_id.
         notification_manager.replace_notification(id, notification);
+    }
+
+    if let Some(timeout) = expire_timeout {
+        notification_manager.set_timeout_for_notification(&id, timeout);
     }
 
     Ok(id)
